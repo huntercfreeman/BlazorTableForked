a820f774e65d357d9faa49cbc4395e2717266c9a
Bernard <bernard.farrell@darkmatter2bd.com>
2020-07-29 14:46:50 -0400

    hey

--- Components/Table.razor.cs
+++ Components/Table.razor.cs
@@ -100,7 +100,7 @@ namespace BlazorTable
 
         protected void NavigateToWorkflows()
         {
-            NewWorkflowText = "<i class='fa fa-spinner fa-spin fa-3x fa-fw'></i>";
+            NewWorkflowText = "Loading...";
             Update();
             NavigationManager.NavigateTo("dtk/wfnew", true);
         }
